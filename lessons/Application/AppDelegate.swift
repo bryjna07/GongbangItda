3bdee7c0b13a19d7b128b72c4ad7394450fbd107
Youngjin <bryjna07@nate.com>
2025-12-11 19:55:23 +0900

    Feat: 프로젝트 초기설정 및 푸시 테스트 #1

---
+++ lessons/Application/AppDelegate.swift
@@ -0,0 +1,75 @@
+//
+//  AppDelegate.swift
+//  lessons
+//
+//  Created by Watson22_YJ on 12/10/25.
+//
+
+import UIKit
+import FirebaseCore
+import FirebaseMessaging
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        // 실기기 빌드 > 권한 문구 허용!
+        FirebaseApp.configure()
+        
+        UNUserNotificationCenter.current().delegate = self
+
+        let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+        UNUserNotificationCenter.current().requestAuthorization(
+          options: authOptions,
+          completionHandler: { _, _ in }
+        )
+
+        application.registerForRemoteNotifications()
+        
+        // 메시지 대리자 설정
+        Messaging.messaging().delegate = self
+        
+        // 현재 등록된 토큰 가져오기
+        Messaging.messaging().token { token, error in
+          if let error = error {
+            print("Error fetching FCM registration token: \(error)")
+          } else if let token = token {
+            print("FCM registration token: \(token)")
+          }
+        }
+        
+        return true
+    }
+    
+}
+
+
+
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    // devicetoken
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        Messaging.messaging().apnsToken = deviceToken
+    }
+}
+
+extension AppDelegate: MessagingDelegate {
+    
+    //디바이스 토큰 정보가 변경이 되면 알려줌
+    // 토큰 갱신 모니터링
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+      print("Firebase registration token: \(String(describing: fcmToken))")
+
+      let dataDict: [String: String] = ["token": fcmToken ?? ""]
+      NotificationCenter.default.post(
+        name: Notification.Name("FCMToken"),
+        object: nil,
+        userInfo: dataDict
+      )
+      // TODO: If necessary send token to application server.
+      // Note: This callback is fired at each app startup and whenever a new token is generated.
+    }
+}
+// APNs -> FCM
+
+// 구글인포, sdk, 푸시설정 킵
---
+++ lessons/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lessons
+//
+//  Created by Watson22_YJ on 12/10/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .orange
+    }
+
+
+}
+
